24d474f591382b98c11854bd083bee0b2dd92786
Danilo A. Da Luz Junior <129161941+daniloluzjr@users.noreply.github.com>
2023-06-09 15:23:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var swiper = new Swiper(".swiper", {
-      slidesPerView: 4, // Define o número de slides visíveis na tela
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1, // Altera o número de slides visíveis para 1
+    spaceBetween: 20, // Define o espaço entre os slides
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3 // Define o número de slides visíveis para 3 em telas com largura menor que 768px
       }
-    });
+    }
   });
-  
+});
